5efee68d91884e645176047525a0efb0fefaf4fd
mauriciopollis <mauriciopollis@gmail.com>
2023-11-15 14:27:29 -0300

    versão quase funcionando

--- main.c
+++ main.c
@@ -34,7 +34,25 @@ int main(void) {
     a = TARVB_remove_filme(a, "Friends S2E16: The One Where Joey Moves Out",1996, t);
     printf("%d\n", TARVB_conta_filmes(a));
 
-    //printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "Friends S1E3: The One with the Thumb", 1994, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+
+    a = TARVB_remove_filme(a, "The Walking Dead S1E3: Tell It to the Frogs", 2010, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "Schindlers List", 1993, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "Gifted Hands: The Ben Carson Story", 2009, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "The Avengers", 2012, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "Dances with Wolves", 1990, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "The Exorcist", 1973, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "The Walking Dead S1E1: Days Gone Bye", 2010, t);
+    printf("%d\n", TARVB_conta_filmes(a));
+    a = TARVB_remove_filme(a, "The Walking Dead S1E2: Guts", 2010, t);
+    printf("%d\n", TARVB_conta_filmes(a));
     a = TARVB_remove_filme(a, "Friends S1E3: The One with the Thumb", 1994, t);
     printf("%d\n", TARVB_conta_filmes(a));
 
